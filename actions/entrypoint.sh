54dee16423ef498f28cab1a6decea79dabe00166
Jeong Han Lee <jeonghan.lee@gmail.com>
2019-01-03 17:39:22 +0100

    add cloning and cd

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -1,3 +1,11 @@
-#!/bin/sh -l
+#!/bin/sh
 
-sh -c "echo Hello Github Action!"
+set -e
+
+echo "Cloning the EPICS BASE source ..."
+git clone  --depth 1 https://github.com/epics-base/epics-base 
+cd epics-base/modules/database/src/std/rec/
+
+ls
+
+
