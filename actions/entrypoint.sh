88c8f3a6240d229a57d1fafcb3edd9d59c9fc1e8
Jeong Han Lee <jeonghan.lee@gmail.com>
2019-01-03 18:24:06 +0100

    test more

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -2,9 +2,18 @@
 
 set -e
 
+echo ""
 echo "Cloning the EPICS BASE source ..."
 git clone  --depth 1 https://github.com/epics-base/epics-base 
-cd epics-base/modules/database/src/std/rec/
+
+
+cd epics-base
+make
+
+
+cd modules/database/src/std/rec/
+
+echo $PWD
 
 ls
 
