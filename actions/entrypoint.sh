5f5145a9baf3179ebcd7a656d074023e7b8abcbc
Jeong Han Lee <jeonghan.lee@gmail.com>
2019-01-03 23:27:05 +0100

    test e3 build

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -2,19 +2,20 @@
 
 set -e
 
+E3_PATH=${HOME}/epics
+BASE=3.15.5
+REQUIRE=3.0.4
+TAG=3.15.5
 echo ""
 echo "Cloning the EPICS BASE source ..."
-git clone  --depth 1 https://github.com/epics-base/epics-base 
+git clone  --depth 1 https://github.com/icshwi/e3 e3-${BASE}
 
+cd e3-${BASE}
+bash e3_building_config.bash -y -t "${E3_PATH}" -b "${BASE}" -r "${REQUIRE}" -c "${TAG}" setup
 
-cd epics-base
-make
+bash e3.bash base
+bash e3.bash req
+bash e3.bash -c mod
 
-
-cd modules/database/src/std/rec/
-
-echo $PWD
-
-ls
 
 
